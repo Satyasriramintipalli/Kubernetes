73cc0b980494b5724b9cec9182d64955afc1a525
Satyasriramintipalli <98574342+Satyasriramintipalli@users.noreply.github.com>
2026-03-23 09:18:19 +0000

    changed go files

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Satya, Go App Running 🚀")
+}
+
 func main() {
+	http.HandleFunc("/", home)
 
-	initDB()
+	// 🔥 THIS IS IMPORTANT
+	http.Handle("/metrics", promhttp.Handler())
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/", loginPage)
-	r.HandleFunc("/dashboard", dashboard)
-	r.HandleFunc("/create", createUser)
-
-	log.Println("Server started on :8080")
-
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", nil)
 }
